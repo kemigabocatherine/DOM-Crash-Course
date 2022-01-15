b75abb763d95aa839afedfc9ece479a563a29dcc
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-01-15 15:50:51 +0300

    query selector and query selector all

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //DOM METHODS
 // get element by id
-document.getElementById('heading');
-console.log(heading);
-console.log(typeof heading);
+// document.getElementById('heading');
+// console.log(heading);
+// console.log(typeof heading);
 
-// Changing html content
-const list = document.getElementById("list");
-list.textContent = "Hi there";
-console.log(list.textContent);
+// // Changing html content //
+// const list = document.getElementById("list");
+// //list.textContent = "Hi there";
+// console.log(list.textContent);
+
+// list.innerHTML = "<h1>Hi there</h1>";
+
+// console.log(list.innerHTML);
+
+// // Qeury selector -->returns first element that matches specified css selector
+
+const list = document.querySelector("#list");
+console.log(list);
+
+const lis = document.querySelectorAll("li");
+console.log(lis);
+
