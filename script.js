3adc1255b89c17e780d8e8efd8681e64217f0884
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-01-15 03:29:00 +0300

    Learn DOM methods and how to change html content

---
+++ script.js
@@ -0,0 +1,10 @@
+//DOM METHODS
+// get element by id
+document.getElementById('heading');
+console.log(heading);
+console.log(typeof heading);
+
+// Changing html content
+const list = document.getElementById("list");
+list.textContent = "Hi there";
+console.log(list.textContent);
