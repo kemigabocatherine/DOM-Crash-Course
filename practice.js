ee4680c921b63dddf1bab87908bb6315ae8c0a28
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-01-21 20:44:17 +0300

    Learning js full course

--- practice.js
+++ practice.js
@@ -30,8 +30,6 @@ function testLogicalOr(val) {
 
 console.log(testLogicalOr(2)); */
 
-const { arrayBuffer } = require("stream/consumers");
-
 // Switch statement
 function caseInSwitch(val) {
     var answer = "";
@@ -176,5 +174,152 @@ var ourTotal = 0;
 for (var i = 0; i < ourArr.length; i++) {
     ourTotal += ourArr[i];
 }
+
 console.log(ourTotal);
+
+// NESTING FOR LOOPS
+
+function multiplyAll(arr) {
+    var product = 1;
+
+    for (var i = 0; i < arr.length; i++) {
+        for (var j=0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+
+    return product;
+}
+
+var product = multiplyAll([1,2],[3,4],[5,6,7]);
+console.log(product);
+
+// Do while loop
+var myArray = [];
+var i = 10;
+
+do {
+    myArray.push(i);
+    i++;
+}while(i < 5)
+
+console.log(i, myArray);
+
+
+// Random fraction
+function randomFraction() {
+    return Math.random();
+}
+
+console.log(randomFraction());
+
+// Random whole numbers
+var randomNumberBetween0and19 = Math.floor(Math.random()*20);
+
+function randomWholeNum() {
+    return Math.random()*20;
+}
+
+console.log(randomWholeNum());
+
+// Random whole numbers in a range
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+console.log(randomRange(5, 15));
+
+// parseInt --> converts a string to an integer
+function convertToInteger(str) {
+    return parseInt(str);
+}
+console.log(convertToInteger("3456"));
+
+// parseInt with a Radix -->tells the base of the number
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+console.log(convertToInteger("10011"));
+
+// Ternary operator
+// function CheckEqual(a, b) {
+//     if(a === b) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// } --> inplace of this 
+
+function CheckEqual(a, b) {
+
+    return a === b ? true : false;
+   
+} 
+
+console.log(CheckEqual(2, 2));
+
+// multiple ternary(conditional) operators
+function CheckSign(num) {
+
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero"
+   
+} 
+
+console.log(CheckSign(0));
+
+/* NB : let --> can't allow you to declare a variable twice
+    --> limitedto the block in which it is declared 
+const --> is read only
+*/
+
+// mutate const variables --> use bracket notation
+const s = [5, 7, 2];
+function editInPlace() {
+    "use strict";
+
+    // s = [2, 5, 7];
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+}
+
+editInPlace();
+console.log(s);
+
+// Prevent object mutation
+//--> Use Object.freeze(object);
+
+// ARROW FUNCTIONS -> to write anonymous functions
+var magic = function() {
+    return new Date;
+};  // anonymous function
+
+//const magic = () => new Date; // same function 
+
+// arrow fuctions with parameters
+// var myConcat = function(arr1, arr2) {
+//     return arr1.concat(arr2);
+// }
+
+// console.log(myConcat([1, 2], [3, 4, 5]));
+// arrow
+var myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+// Arrow functions for higher end functions
+// Higher end functions --> map, filter, reduce
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    return squaredIntegers;
+}
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
 
